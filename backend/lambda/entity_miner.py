5fd9d5542f07e096f40587dd92c976e181338c16
boundlesslightbringer <kunae47@gmail.com>
2025-12-19 13:10:01 +0530

    added provisional lambda handling for S3 triggers; added full lambda handling for manual invocation

--- backend/lambda/entity_miner.py
+++ backend/lambda/entity_miner.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import re
 import logging
@@ -14,11 +15,6 @@ import os
 logger = logging.getLogger()
 # logger.setLevel(logging.INFO)
 logging.basicConfig(level=logging.INFO)
-
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
 
 
 class GenreDetermination(BaseModel):
@@ -189,11 +185,8 @@ class OrganizationProfile(BaseModel):
 # class RelationshipExtractor(BaseModel):
 #     relationships: list[ExtractedRelationships]
 
-
-class EntityMiningWorkflow:
-    def __init__(self):
-        logger.info("Initializing EntityMiningWorkflow")
-        # Try to find config.json in the current directory or lambda directory
+def load_config():
+    try:
         config_path = "config.json"
         if not os.path.exists(config_path):
             # Try lambda directory
@@ -204,9 +197,28 @@ class EntityMiningWorkflow:
                 raise FileNotFoundError(
                     f"config.json not found in current directory or {os.path.dirname(__file__)}"
                 )
+    except Exception as e:
+        logger.error(f"Error loading config: {e}")
+        raise
 
-        with open(config_path, "r") as f:
-            self.config = json.load(f)
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    return config
+
+class EntityMiningWorkflow:
+    def __init__(
+        self,
+        novel_name: str,
+        chroma_collection_name: str,
+        local_chroma: bool = False,
+        model_temperature: float = 0,
+        model_top_p: float = 0.9,
+        model_seed: int = 69420,
+    ):
+        logger.info("Initializing EntityMiningWorkflow")
+
+        self.config = load_config()
 
         dynamodb = boto3.resource(
             "dynamodb", region_name=self.config.get("aws").get("region")
@@ -227,15 +239,22 @@ class EntityMiningWorkflow:
         self.global_prompt_templates_novel_name = self.config.get("aws").get(
             "dynamodb_table_global_prompt_templates_novel_name"
         )
-        model_cfg = self.config.get("model", {})
-        self.model_temperature = model_cfg.get("temperature", 0)
-        self.model_top_p = model_cfg.get("top_p", 0.9)
-        self.model_seed = model_cfg.get("seed", 69420)
+
+        self.model_temperature = self.model_temperature
+        self.model_top_p = self.model_top_p
+        self.model_seed = self.model_seed
+
         self.fetch_workflow_prompt_templates()
-        self.initialize_chroma(local=True)
+
+        self.initialize_chroma(
+            local=local_chroma, collection_name=chroma_collection_name
+        )
+
         logger.info("EntityMiningWorkflow initialized")
 
-    def initialize_chroma(self, local: bool = False) -> None:
+    def initialize_chroma(
+        self, local: bool = False, collection_name: str = None
+    ) -> None:
         try:
             if local:
                 self.chroma_client = chromadb.HttpClient(
@@ -246,18 +265,21 @@ class EntityMiningWorkflow:
                 self.chroma_client = chromadb.HttpClient(
                     host=self.config.get("chroma").get("host"),
                     port=self.config.get("chroma").get("port"),
-                    settings=chromadb.Settings(anonymized_telemetry=False)
+                    settings=chromadb.Settings(anonymized_telemetry=False),
                 )
         except Exception as e:
             logger.error(f"Error initializing ChromaDB: {e}")
             raise e
 
         try:
+            if not collection_name:
+                collection_name = self.config.get("chroma").get("default_collection")
+
             self.chroma_collection = self.chroma_client.get_or_create_collection(
-                name=self.config.get("chroma").get("default_collection")
+                name=collection_name
             )
         except Exception as e:
-            logger.error(f"Error getting ChromaDB collection: {e}")
+            logger.error(f"Error fetching or creating ChromaDB collection: {e}")
             raise e
 
     def fetch_workflow_prompt_templates(self) -> None:
@@ -382,7 +404,7 @@ class EntityMiningWorkflow:
             .get("content")
         )
         # logger.info(f"Response content:\n{content}\n")
-        
+
         extracted_json = content.split("```json")[-1].split("```")[0]
 
         parsed = json.loads(extracted_json)
@@ -399,7 +421,7 @@ class EntityMiningWorkflow:
 
         # if matches:
         #     extracted_json = matches[-1].strip()
-            
+
         #     try:
         #         # Parse to ensure it's valid JSON
         #         parsed = json.loads(extracted_json)
@@ -464,7 +486,12 @@ class EntityMiningWorkflow:
         return self._parse_response(response, EntityExtractionAndClassification)
 
     def profile_entity(
-        self, text: str, entity_name: str, genre: str, category: str, significance: str = None
+        self,
+        text: str,
+        entity_name: str,
+        genre: str,
+        category: str,
+        significance: str = None,
     ) -> BaseModel:
         if category not in self.profile_config:
             logger.warning(f"No profiler configured for category: {category}")
@@ -475,13 +502,15 @@ class EntityMiningWorkflow:
             config["template"], config["label"]
         )
 
-
-        if category == "Person": 
+        if category == "Person":
             if significance == "Minor":
                 pass
             else:
                 instruction_prompt = instruction_prompt.format(
-                    entity_name=entity_name, genre=genre, text=text, significance=significance
+                    entity_name=entity_name,
+                    genre=genre,
+                    text=text,
+                    significance=significance,
                 )
         else:
             instruction_prompt = instruction_prompt.format(
@@ -489,9 +518,9 @@ class EntityMiningWorkflow:
             )
 
         response = self.invoke_model(
-        model_id=self.model_id,
-        system_prompt=system_prompt,
-        instruction_prompt=instruction_prompt,
+            model_id=self.model_id,
+            system_prompt=system_prompt,
+            instruction_prompt=instruction_prompt,
         )
 
         return self._parse_response(response, config["schema"])
@@ -526,7 +555,12 @@ class EntityMiningWorkflow:
         ) as executor:
             futures = [
                 executor.submit(
-                    self.profile_entity, text=text, entity_name=entity.name, genre=genre, category=entity.category, significance=entity.significance if entity.significance else None
+                    self.profile_entity,
+                    text=text,
+                    entity_name=entity.name,
+                    genre=genre,
+                    category=entity.category,
+                    significance=entity.significance if entity.significance else None,
                 )
                 for entity in extracted_entities.entities
             ]
@@ -539,31 +573,110 @@ class EntityMiningWorkflow:
                     logger.error(f"Error profiling entity: {e}")
 
         # relationships = self.extract_relationships(text) TODO: need a separate process for extracting and determining the relationships between the entities
+
         logger.info("Entity mining execution completed")
         return {
             "genre": genre,
             "genre_determination_reasoning": genre_result.reasoning,
             "profiled_entities": profiled_entities,
-            # "relationships": relationships
         }
 
     def save_entities_to_chroma(
         self, entity_profiles: list[BaseModel], genre: str, novel_name: str
-    ) -> None:
-        doc_ids = []
-        for profile in entity_profiles:
-            try:
-                doc_id = f"{novel_name}-{profile.name}"
-            except AttributeError as ae:
-                doc_id = f"{novel_name}-{profile.primary_name}"
-            doc_ids.append(doc_id)
-                
-        self.chroma_collection.add(
-            documents=[profile.model_dump_json() for profile in entity_profiles],
-            metadatas=[{"genre": genre, "novel_name": novel_name, "source": "entity_miner"} for profile in entity_profiles],
-            ids=doc_ids,
-        )
-        logger.info(f"Saved {len(entity_profiles)} entity profiles to ChromaDB")
+    ) -> bool:
+        try:
+            doc_ids = []
+            for profile in entity_profiles:
+                try:
+                    doc_id = f"{novel_name}-{profile.name}"
+                except AttributeError as ae:
+                    doc_id = f"{novel_name}-{profile.primary_name}"
+                doc_ids.append(doc_id)
+
+            self.chroma_collection.add(
+                documents=[profile.model_dump_json() for profile in entity_profiles],
+                metadatas=[
+                    {
+                        "novel_name": novel_name,
+                        "genre": genre,
+                        "source": "entity_miner",
+                        "created_at": datetime.now().isoformat(),
+                    }
+                    for profile in entity_profiles
+                ],
+                ids=doc_ids,
+            )
+            logger.info(f"Saved {len(entity_profiles)} entity profiles to ChromaDB")
+        except Exception as e:
+            logger.error(f"Error saving entities to ChromaDB: {e}")
+            return False
+
+        return True
+
+
+def lambda_handler(event, context):
+    """
+    event should contain a dictionary with the following keys:
+    - novel_name: str
+    - text: str
+    - username: str
+    """
+    result = None
+
+    # check if event source is S3 trigger
+    if "Records" in event and event["Records"][0]["eventSource"] == "aws:s3":
+        # get the S3 bucket name and key
+        bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+        key = event["Records"][0]["s3"]["object"]["key"]
+        event_type = event["Records"][0]["eventName"]
+
+
+        config = load_config()
+        s3_client = boto3.client("s3", region_name=config.get("aws").get("region"))
+        try:
+            file_object = s3_client.get_object(Bucket=bucket_name, Key=key)
+            file_object_metadata = file_object["Metadata"]
+            story_text = file_object["Body"].read().decode("utf-8")
+            novel_name = key.split("/")[-1].split(".")[0]
+            username = file_object_metadata.get("username")
+            last_modified_ts = file_object_metadata.get("last_modified")
+            story_text_hash = file_object_metadata.get("story_text_hash")
+
+            if story_text_hash == hashlib.sha256(story_text.encode("utf-8")).hexdigest() or last_modified_ts < datetime.now() - datetime.timedelta(minutes=10):
+                logger.warning(f"Story text has not changed since last modification or last modification is more than 10 minutes ago")
+                return {
+                    "status": "no change",
+                    "num_mined_entities": 0,
+                }
+
+        except s3_client.exceptions.NoSuchKey:
+            raise ValueError(f"Object '{key}' not found in bucket '{bucket_name}'")
+        except Exception as e:
+            logger.error(f"S3 Error: {e}")
+            raise ValueError(f"S3 Error: {e}")
+
+
+    # synchronous trigger called via React frontend
+    else:  
+        story_text = event.get("text")
+        novel_name = event.get("novel_name")
+        username = event.get("username")
+
+    entity_miner = EntityMiningWorkflow(
+        novel_name=novel_name, chroma_collection_name=f"{username}-{novel_name}"
+    )
+
+    mined_entities = entity_miner.execute(story_text)
+    saved_to_chroma = entity_miner.save_entities_to_chroma(
+        mined_entities["profiled_entities"], mined_entities["genre"], novel_name
+    )
+
+    result = {
+        "status": "success" if saved_to_chroma else "error",
+        "num_mined_entities": len(mined_entities["profiled_entities"]),
+    }
+
+    return result
 
 
 if __name__ == "__main__":
@@ -571,7 +684,9 @@ if __name__ == "__main__":
     prologue_path = "nadarr_prologue.txt"
     if not os.path.exists(prologue_path):
         # Try lambda directory
-        lambda_prologue_path = os.path.join(os.path.dirname(__file__), "nadarr_prologue.txt")
+        lambda_prologue_path = os.path.join(
+            os.path.dirname(__file__), "nadarr_prologue.txt"
+        )
         if os.path.exists(lambda_prologue_path):
             prologue_path = lambda_prologue_path
         else:
@@ -579,7 +694,7 @@ if __name__ == "__main__":
             stories_prologue_path = os.path.join(
                 os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
                 "stories",
-                "nadarr_prologue.txt"
+                "nadarr_prologue.txt",
             )
             if os.path.exists(stories_prologue_path):
                 prologue_path = stories_prologue_path
@@ -587,10 +702,12 @@ if __name__ == "__main__":
                 raise FileNotFoundError(
                     f"nadarr_prologue.txt not found in current directory, {os.path.dirname(__file__)}, or {stories_prologue_path}"
                 )
-    
+
     with open(prologue_path, "r") as f:
         text = f.read()
     entity_miner = EntityMiningWorkflow()
     result = entity_miner.execute(text)
     # print(result)
-    entity_miner.save_entities_to_chroma(result["profiled_entities"], result["genre"], "abs_prologue")
+    entity_miner.save_entities_to_chroma(
+        result["profiled_entities"], result["genre"], "abs_prologue"
+    )
